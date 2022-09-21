27f30acb65265a179560fc7068c86d3036ab6a09
vladimir <vladimir@mail.ru>
2022-09-21 18:13:22 +0300

    End Task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("345 Hello, World!");
+﻿// Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = a % 2;
+
+if (b == 0)
+{
+Console.WriteLine("Число является четным");
+}
+else
+{
+Console.WriteLine("Число является нечетным");
+}
