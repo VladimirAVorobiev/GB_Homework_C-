c9d98d9ba62866e0c992d62c779fef61f4b8c00d
vladimir <vladimir@mail.ru>
2022-09-21 18:35:12 +0300

    End Task8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Task 8 Hello, World!");
+﻿// Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите целое положительное число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int even_number = 2;
+
+if (N <= 0)
+{
+Console.WriteLine("Введенное число не является положительным");    
+}
+else
+{
+    while (even_number <= N)
+    {
+        Console.WriteLine(even_number);
+        even_number += 2;
+    }
+}
