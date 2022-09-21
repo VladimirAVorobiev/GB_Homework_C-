07ace56515a0b29989f45566254c625cc7957b9c
vladimir <vladimir@mail.ru>
2022-09-21 17:42:42 +0300

    End Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! ! !");
+﻿// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите два целых числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("Числа равны", a);
+}
+else
+{
+    if (a > b)
+    {
+        Console.WriteLine("Большее число");
+        Console.WriteLine(a);
+        Console.WriteLine("Меньшее число");
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.WriteLine("Большее число", b);
+        Console.WriteLine(b);
+        Console.WriteLine("Меньшее число", a);
+        Console.WriteLine(a);
+    }
+}
