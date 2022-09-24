2ddd22aa5eb7625a5a0e00029419358c545e397d
vladimir <vladimir@mail.ru>
2022-09-24 14:54:15 +0300

    End Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Task 10 Hello, World!");
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int GetNumber()
+
+{
+    Console.WriteLine("Введите трехзначное целое число");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SecondDigit(int number)
+{
+int second;
+second = number % 100;
+Console.WriteLine(second / 10);
+}
+
+SecondDigit(GetNumber());
