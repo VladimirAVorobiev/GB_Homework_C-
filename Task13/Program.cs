465599e70fb8605036894db9390ae742ba28496a
vladimir <vladimir@mail.ru>
2022-09-24 15:07:50 +0300

    End Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Task 13 Hello, World!");
+﻿// Программа,которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+int GetNumber()
+
+{
+    Console.WriteLine("Введите целое число");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void ThirdDigit(int number)
+{
+int third;
+    if (number < 100) Console.WriteLine("Число не иммет третьей цифры");
+else
+{
+third = number % 1000;
+Console.WriteLine("Третья цифра числа = " + third / 100);
+}
+
+}
+ThirdDigit(GetNumber());
