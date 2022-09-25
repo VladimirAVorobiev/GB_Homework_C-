8f0f74f4b7640020bb142ad3d6c8da227574803c
vladimir <vladimir@mail.ru>
2022-09-25 14:57:51 +0300

    Finish Task 23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
-Console.WriteLine("Task 23 Hello, World!");
+
+int GetNumber()
+{
+    Console.WriteLine("Введите целое число");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void RowOfCubes(int number)
+{
+    int count = 1;
+Console.WriteLine("Ряд кубов чисел от 1 до введенного числа");
+    while (count <= number)
+    {
+        Console.Write(" " + Math.Pow((count),3));
+        count ++;
+    }
+}
+
+RowOfCubes(GetNumber());
