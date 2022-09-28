f34022a43a29d66ebc319b42aa531884de2ae6bb
vladimir <vladimir@mail.ru>
2022-09-28 14:32:47 +0300

    Finish Task 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Task25 Hello, World!");
+﻿// Программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B с помощью цикла
+
+
+double GetNumber()
+{
+    Console.WriteLine("Введите число A");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+int GetStepen()
+{
+    Console.WriteLine("Введите целое число N - показатель степени");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PowOfNumber(double number, int stepen)
+{
+    double numberinstepen = 1;
+
+    for (int i = 1; i <= stepen; i++)
+
+    {
+        numberinstepen = numberinstepen * number;
+    }
+
+    Console.WriteLine("Число A в степени N = " + numberinstepen);
+
+}
+
+double number = GetNumber();
+int stepen = GetStepen();
+PowOfNumber(number, stepen);
