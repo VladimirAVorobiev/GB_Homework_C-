c3a86bfa0c7ae2ebfd75dd652a71891f723f55ee
vladimir <vladimir@mail.ru>
2022-09-29 10:06:18 +0300

    Finish Task27

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Task29 Hello, World!");
+﻿// Программа, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void GetNumbersArr(int[] arr)
+{
+    Console.WriteLine("Введите целые числа - элементы массива!");
+
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[] arr)
+{
+       for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + "  ");
+    }
+}
+
+int[] array = new int[8];
+
+GetNumbersArr(array);
+Console.WriteLine();
+PrintArray(array);
