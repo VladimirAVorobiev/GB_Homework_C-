ce1c810e08587d41c88b3b0ee30228a6a919bf53
vladimir <vladimir@mail.ru>
2022-10-02 13:11:07 +0300

    Finish Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,5 +1,30 @@
 ﻿// Программа, которая задает массив, заполненный случайными положительными трёхзначными числами,
 // и показывает количество чётных чисел в массиве.
 
-Console.WriteLine("Task 34 Hello, World  !  ");
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine(" ");
+}
 
+void CheckEvenNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            sum++;
+    }
+    Console.WriteLine("Количество четных элементов массива равно " + sum);
+}
+
+Console.WriteLine("Введите целое число - количество элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Console.WriteLine(" ");
+FillArray(array);
+CheckEvenNumbers(array);
