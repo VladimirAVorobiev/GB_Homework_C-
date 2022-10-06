87b9760f7aa973d46aa17e850329b2553f78025a
vladimir <vladimir@mail.ru>
2022-10-06 09:50:29 +0300

    Finish Task41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -2,4 +2,28 @@
 // и производит подсчет, сколько чисел больше 0 ввёл пользователь.
 
 
-Console.WriteLine("Task 41 Hello, World!");
+void FillArrayUsersNumbers(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-е число");
+        arr[i] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+void CheckAndPrint(double[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) { count++; }
+    }
+    Console.WriteLine($"Вы ввели {count} положительных числа");
+}
+
+Console.WriteLine("Введите целое число - количество чисел, которые вы планируете вносить");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+
+FillArrayUsersNumbers(array);
+CheckAndPrint(array);
