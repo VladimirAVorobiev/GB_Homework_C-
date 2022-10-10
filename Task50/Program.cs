5b4fe12f50eb6cad75e46afbfbcd6bd5c5444132
vladimir <vladimir@mail.ru>
2022-10-10 16:14:58 +0300

    Finish Task50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Task 50 Hello, World!");
+﻿// Программу, которая рандомно создает массив n х m. 
+// Позволяет пользователю ввести значения n и m для поиска элемента массива.
+// Возвращает значение этого элемента по ввеным значениям для поиска или же указание, что такого элемента нет.
+
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillRandomValueAndPrintArray(double[,] arr, int minValue, int maxValue)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue + 1) * new Random().NextDouble();
+            Console.Write(arr[i, j] + " "); // Используем эту строку для контроля корректности результатов
+        }
+        Console.WriteLine(); // Используем эту строку для контроля корректности результатов
+    }
+}
+
+double[,] CreationArray(int m, int n)
+{
+    return new double[m, n];
+}
+
+void SearchElementOfArray(double[,] arr, int xelement, int yelement)
+{
+    if (xelement > arr.GetLength(1) || yelement > arr.GetLength(0))
+    { Console.WriteLine("Найти элеменет невозможно, так как вы ввели значения индексов элемента выходящие за объявленные размеры массива"); }
+    else Console.WriteLine($"Значение найденного элемента равно {arr[yelement - 1, xelement - 1]}");
+}
+
+int m = GetNumberFromConsole("Введите количество строк в массиве");
+int n = GetNumberFromConsole("Введите количество столбцов в массиве");
+int minValue = GetNumberFromConsole("Введите минимальное значение элементов массива");
+int mахValue = GetNumberFromConsole("Введите максимальное значение элементов массива");
+int x = GetNumberFromConsole("Введите номер столбца искомого элемента массива");
+int y = GetNumberFromConsole("Введите номер строки искомого элемента массива");
+
+
+double[,] array = CreationArray(m, n);
+FillRandomValueAndPrintArray(array, minValue, mахValue);
+Console.WriteLine();
+SearchElementOfArray(array, x, y);
+
--- Task52/Program.cs
+++ Task52/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Task 52 Hello, World!");
+﻿// Программа, которая создает двумерный массив из целых чисел 
+// и находит среднее арифметическое элементов в каждом столбце.
+
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillRandomValueAndPrintArray(int[,] arr, int minValue, int maxValue)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue + 1);
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreationArray(int m, int n)
+{
+    return new int[m, n];
+}
+
+void GetColumnAverage(int[,] arr)
+{
+    double average = 0;
+       for (int j = 0; j < arr.GetLength(1); j++)
+    {
+         int sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum = sum + arr[i, j];
+        }
+        average = Convert.ToDouble(sum / arr.GetLength(0));   // Почему то не переводится в формат double
+        Console.WriteLine($"Среднее арифметическое столбца {j+1} = {average}");            
+    }
+}
+
+int m = GetNumberFromConsole("Введите количество строк в массиве");
+int n = GetNumberFromConsole("Введите количество столбцов в массиве");
+int minValue = GetNumberFromConsole("Введите минимальное значение элементов массива");
+int mахValue = GetNumberFromConsole("Введите максимальное значение элементов массива");
+
+int[,] array = CreationArray(m, n);
+FillRandomValueAndPrintArray(array, minValue, mахValue);
+GetColumnAverage(array);
+
