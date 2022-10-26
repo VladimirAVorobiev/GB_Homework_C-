5013ffb68522e012ce28ed8d1c1f3ac02aecae7a
vladimir <vladimir@mail.ru>
2022-10-26 15:29:46 +0300

    Finish Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая позволяет задать значение N, выводит все натуральные числа в промежутке от N до 1,
+// с помощью рекурсии.
+
+double GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+void WriteNumbersRecursionVoid(int startNumber, double lastNumber)
+{
+    if (lastNumber >= startNumber)
+    {
+        Console.Write(lastNumber + " ");
+        WriteNumbersRecursionVoid(startNumber, lastNumber-1);
+    }     
+}
+
+double number = Math.Floor(GetNumberFromConsole("Введите число N"));
+WriteNumbersRecursionVoid(1, number);
